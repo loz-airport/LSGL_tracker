de1b66630632c0873ceca5202bfe50b81ed6e032
loz-airport <lsgl.jc5kz@simplelogin.com>
2025-12-26 00:57:18 +0100

    refactor: Move data CSVs from `data/` to `data_raw/` and update `_helper.R`.

--- R/_helper.R
+++ R/_helper.R
@@ -1,4 +1,4 @@
-#library(tidyverse)
+# library(tidyverse)
 library(dplyr)
 library(stringr)
 library(readr)
@@ -9,11 +9,10 @@ library(lubridate)
 # usr <-  Sys.getenv("usr_osn")
 # pwd <-  Sys.getenv("pwd_osn")
 
-usr <-  Sys.getenv("OPENSKY_USR")
-pwd <-  Sys.getenv("OPENSKY_PWD")
+usr <- Sys.getenv("OPENSKY_USR")
+pwd <- Sys.getenv("OPENSKY_PWD")
 
-cat ("\n\nCheck GT secret, remove me!!! usr: ", usr)
-
+cat("\n\nCheck GT secret, remove me!!! usr: ", usr)
 
 
 get_osf_details <- function(ii, timez = "CET") {
@@ -21,9 +20,9 @@ get_osf_details <- function(ii, timez = "CET") {
     ICAO24 = ii$ICAO24,
     call_sign = ii$call_sign,
     departure_time = with_tz(ii$departure_time, timez),
-    departure_date = as.Date(departure_time,tz = timez),
+    departure_date = as.Date(departure_time, tz = timez),
     arrival_time = with_tz(ii$arrival_time, timez),
-    arrival_date = as.Date(arrival_time,tz = timez),
+    arrival_date = as.Date(arrival_time, tz = timez),
     departure_airport_ICAO = ii$origin_airport,
     destination_airport_ICAO = ii$destination_airport,
     id = str_c(ICAO24, "_", departure_time)
@@ -31,25 +30,27 @@ get_osf_details <- function(ii, timez = "CET") {
 }
 
 os_airport2df <- function(airl) {
-  1:length(airl) %>%
+  seq_along(airl) %>%
     map_df(function(iii) {
       get_osf_details(airl[[iii]])
-    }) %>% 
+    }) %>%
     arrange(departure_time)
 }
 
 
 os_aircraft_SV_2df <- function(aircr, timez = "CET") {
-  1:length(aircr) %>%
+  seq_along(aircr) %>%
     map_df(function(ii) {
-      
-      if(!is.null(aircr[[ii]])) {
+      if (!is.null(aircr[[ii]])) {
         aircr[[ii]]$get_values(
-          c("ICAO24",
-            "longitude", "latitude", "requested_time", 
-            "geo_altitude", "velocity", 
-            "special_purpose_indicator", "origin_country")) %>% 
-          as_tibble() %>% 
+          c(
+            "ICAO24",
+            "longitude", "latitude", "requested_time",
+            "geo_altitude", "velocity",
+            "special_purpose_indicator", "origin_country"
+          )
+        ) %>%
+          as_tibble() %>%
           mutate(idx = ii)
       } else {
         NULL
@@ -58,61 +59,71 @@ os_aircraft_SV_2df <- function(aircr, timez = "CET") {
 }
 
 
-# 
-# ldf <- bl_dep_df %>% 
+#
+# ldf <- bl_dep_df %>%
 #   group_split(departure_date)
 # base_file_name <- "bl_dep"
-# output_dir <- "data"
-# ii <- 1 
+# output_dir <- "data_raw"
+# ii <- 1
 
 # Take a list data.frame, split by date & save them by date
 ldf2files <- function(
-  ldf, 
-  base_file_name ="bl_dep",
-  output_dir = "data",
+  ldf,
+  base_file_name = "bl_dep",
+  output_dir = "data_raw",
   verbose = F
-  ) {
-  if(length(ldf) > 0) {
-    1:length(ldf) %>% 
+) {
+  if (length(ldf) > 0) {
+    seq_along(ldf) %>%
       map(function(ii) {
-        
-        tdate <- ldf[[ii]]$departure_date %>% unique() %>% max()
-        if(length(tdate) > 1) {
-          warning ("\nThis chunk has different dates!\n")
+        tdate <- ldf[[ii]]$departure_date %>%
+          unique() %>%
+          max()
+        if (length(tdate) > 1) {
+          warning("\nThis chunk has different dates!\n")
         }
-        
-        out_file <- str_c(output_dir, "/", base_file_name, "_",
-                          format(tdate, "%Y_%m_%d"), ".csv")
-        if(file.exists(out_file)) {
-          tmp_read <- read_csv(out_file, show_col_types = F) 
+
+        out_file <- str_c(
+          output_dir, "/", base_file_name, "_",
+          format(tdate, "%Y_%m_%d"), ".csv"
+        )
+        if (file.exists(out_file)) {
+          tmp_read <- read_csv(out_file, show_col_types = F)
           tmp_nrow <- tmp_read %>% nrow()
         } else {
           tmp_nrow <- 0
         }
-        if(nrow(ldf[[ii]]) > tmp_nrow) {
-          if(verbose) cat(
-            "\n nrow(ldf[[ii]]: ", nrow(ldf[[ii]]), " > ", 
-            tmp_nrow, " (tmp_nrow)")
-          
-          ldf[[ii]] %>% 
+        if (nrow(ldf[[ii]]) > tmp_nrow) {
+          if (verbose) {
+            cat(
+              "\n nrow(ldf[[ii]]: ", nrow(ldf[[ii]]), " > ",
+              tmp_nrow, " (tmp_nrow)"
+            )
+          }
+
+          ldf[[ii]] %>%
             write_csv(out_file)
         } else {
-          if(verbose) cat("\nNothing to save because tmp_nrow:", tmp_nrow,
-                          "\tcurrently fetched: ", nrow(ldf[[ii]]), " rows")
-        } 
+          if (verbose) {
+            cat(
+              "\nNothing to save because tmp_nrow:", tmp_nrow,
+              "\tcurrently fetched: ", nrow(ldf[[ii]]), " rows"
+            )
+          }
+        }
       })
   }
 }
 
 checkFileData <- function(out_file, nrow_threshold = 1) {
   tmp_nrow <- 0
-  if(file.exists(out_file)) {
-    tmp_read <- read_csv(out_file, show_col_types = F) 
+  if (file.exists(out_file)) {
+    tmp_read <- read_csv(out_file, show_col_types = F)
     tmp_nrow <- tmp_read %>% nrow()
   } else {
     tmp_nrow <- 0
   }
-  if(tmp_nrow > nrow_threshold) {
+  if (tmp_nrow > nrow_threshold) {
     T
   } else {
     F
@@ -120,31 +131,35 @@ checkFileData <- function(out_file, nrow_threshold = 1) {
 }
 
 DatesMissingData <- function(
-  qDates, 
+  qDates,
   base_file_name = c("bl_dep", "bl_arr", "bl_dep_SV", "bl_arr_SV"),
-  output_dir = "data",
+  output_dir = "data_raw",
   verbose = F
 ) {
   stopifnot(is.Date(qDates))
-  
-  out_files <- qDates %>% 
+
+  out_files <- qDates %>%
     map_df(function(dd) {
-      out_files <- str_c(output_dir, "/", base_file_name, "_",
-            format(dd, "%Y_%m_%d"), ".csv")
+      out_files <- str_c(
+        output_dir, "/", base_file_name, "_",
+        format(dd, "%Y_%m_%d"), ".csv"
+      )
       tibble(
         date = dd,
         dep = out_files[1], arr = out_files[2],
-        depSV = out_files[3], arrSV = out_files[4])
-    }) 
-  out_check <- 1:nrow(out_files) %>% 
+        depSV = out_files[3], arrSV = out_files[4]
+      )
+    })
+  out_check <- seq_len(nrow(out_files)) %>%
     map_lgl(function(ii) {
-      out_files %>% 
-        slice(ii) %>% 
-        select(-date) %>% 
-        unlist() %>% map_lgl(checkFileData, nrow_threshold = 0) %>% 
+      out_files %>%
+        slice(ii) %>%
+        select(-date) %>%
+        unlist() %>%
+        map_lgl(checkFileData, nrow_threshold = 0) %>%
         all()
-    }) 
-  
+    })
+
   qDates[!out_check]
 }
 
@@ -152,37 +167,36 @@ DatesMissingData <- function(
 ## main wrapper get and save arrival/departurs
 
 getSaveArrivalDeparture <- function(
-  start, 
+  start,
   end,
   airport = "LSGL",
-  tz  = "Europe/Zurich",
+  tz = "Europe/Zurich",
   timeRes = 180,
   timeOut = 7,
   verbose = F
 ) {
-
   stopifnot(exists("usr"))
   stopifnot(exists("pwd"))
-  
-  # Get airport arrivals/departures without state vectors 
+
+  # Get airport arrivals/departures without state vectors
   # Get stateVectors separately
 
-  if(verbose) cat("\n\nGET ",  airport, " stuff from ", start, " until ", end)  
-  if(verbose) cat("\nGet ",  airport, " departures")
+  if (verbose) cat("\n\nGET ", airport, " stuff from ", start, " until ", end)
+  if (verbose) cat("\nGet ", airport, " departures")
   ### Departures
-  bl_dep<- getAirportDepartures(
-    airport=airport,
-    endTime=end,
-    startTime=start,
+  bl_dep <- getAirportDepartures(
+    airport = airport,
+    endTime = end,
+    startTime = start,
     timeZone = tz
   )
-  if(!is.null(bl_dep) & length(bl_dep) > 0) {
+  if (!is.null(bl_dep) & length(bl_dep) > 0) {
     bl_dep_df <- os_airport2df(bl_dep)
     # SV
-    if(verbose) cat("\n\tGet for these departures the corresponding state vectors")
-    bl_dep_sv_l <- 1:nrow(bl_dep_df) %>% 
+    if (verbose) cat("\n\tGet for these departures the corresponding state vectors")
+    bl_dep_sv_l <- seq_len(nrow(bl_dep_df)) %>%
       map(function(ii) {
-        if(verbose) cat("\tFetching SV for ", ii, "/", nrow(bl_dep_df))
+        if (verbose) cat("\tFetching SV for ", ii, "/", nrow(bl_dep_df))
         getAircraftStateVectorsSeries(
           bl_dep_df$ICAO24[ii],
           bl_dep_df$departure_time[ii],
@@ -194,60 +208,58 @@ getSaveArrivalDeparture <- function(
           useImpalaShell = T,
           timeOut = timeOut,
           maxQueryAttempts = 2
-        ) 
+        )
       })
     bl_dep_sv_df <- os_aircraft_SV_2df(bl_dep_sv_l)
     bl_dep_sv_df <- left_join(
       bl_dep_sv_df,
-      bl_dep_df %>% 
-        mutate(idx = row_number()) %>% 
+      bl_dep_df %>%
+        mutate(idx = row_number()) %>%
         select(idx, id)
-    ) %>% 
+    ) %>%
       select(-idx)
-    
+
     stopifnot(all(unique(bl_dep_sv_df$id) %in% bl_dep_df$id))
-    
+
     # Save each day as a different file
-    if(verbose) cat("\nSave as different files")
-    
+    if (verbose) cat("\nSave as different files")
+
     ldf2files(
-      ldf = bl_dep_df %>% 
+      ldf = bl_dep_df %>%
         group_split(departure_date),
       base_file_name <- "bl_dep",
       verbose = verbose
     )
-    
-    if(exists("bl_dep_sv_df")) {
+
+    if (exists("bl_dep_sv_df")) {
       ldf2files(
-        ldf = bl_dep_sv_df %>% 
-          left_join(bl_dep_df %>% select(arrival_date, departure_date, id)) %>% 
+        ldf = bl_dep_sv_df %>%
+          left_join(bl_dep_df %>% select(arrival_date, departure_date, id)) %>%
           group_split(departure_date),
         base_file_name <- "bl_dep_SV",
         verbose = verbose
-      )  
+      )
     }
+  }
 
-  } 
 
-
-  if(verbose) cat("\nGet ",  airport, " arrivals")
-  ### Arrivals 
+  if (verbose) cat("\nGet ", airport, " arrivals")
+  ### Arrivals
   bl_arr <- getAirportArrivals(
-    airport=airport,
-    endTime=end,
-    startTime=start,
+    airport = airport,
+    endTime = end,
+    startTime = start,
     timeZone = tz
   )
-  
-  if(!is.null(bl_arr) & length(bl_arr) > 0) {
-    
-    bl_arr_df <- os_airport2df(bl_arr) 
-    
+
+  if (!is.null(bl_arr) & length(bl_arr) > 0) {
+    bl_arr_df <- os_airport2df(bl_arr)
+
     # SV
-    if(verbose) cat("\n\tGet for these arrivals the corresponding state vectors")
-    bl_arr_sv_l <- 1:nrow(bl_arr_df) %>% 
+    if (verbose) cat("\n\tGet for these arrivals the corresponding state vectors")
+    bl_arr_sv_l <- seq_len(nrow(bl_arr_df)) %>%
       map(function(ii) {
-        if(verbose) cat("\tFetching SV for ", ii, "/", nrow(bl_arr_df))
+        if (verbose) cat("\tFetching SV for ", ii, "/", nrow(bl_arr_df))
         getAircraftStateVectorsSeries(
           bl_arr_df$ICAO24[ii],
           bl_arr_df$departure_time[ii],
@@ -259,44 +271,43 @@ getSaveArrivalDeparture <- function(
           useImpalaShell = T,
           timeOut = timeOut,
           maxQueryAttempts = 2
-        ) 
+        )
       })
-    
+
     bl_arr_sv_df <- os_aircraft_SV_2df(bl_arr_sv_l)
-    
+
     bl_arr_sv_df <- left_join(
       bl_arr_sv_df,
-      bl_arr_df %>% 
-        mutate(idx = row_number()) %>% 
+      bl_arr_df %>%
+        mutate(idx = row_number()) %>%
         select(idx, id)
-    ) %>% 
+    ) %>%
       select(-idx)
-    
+
     stopifnot(all(unique(bl_arr_sv_df$id) %in% bl_arr_df$id))
-    
+
     # Save each day as a different file
     ldf2files(
-      ldf = bl_arr_df %>% 
+      ldf = bl_arr_df %>%
         group_split(arrival_date),
       base_file_name <- "bl_arr"
     )
-    
-    if(exists("bl_arr_sv_df")) {
+
+    if (exists("bl_arr_sv_df")) {
       ldf2files(
-        ldf = bl_arr_sv_df %>% 
-          left_join(bl_arr_df %>% select(arrival_date, departure_date, id)) %>% 
+        ldf = bl_arr_sv_df %>%
+          left_join(bl_arr_df %>% select(arrival_date, departure_date, id)) %>%
           group_split(arrival_date),
         base_file_name <- "bl_arr_SV"
       )
     }
-    
   }
 }
 
 # concatenate files as a single data.frame
 concatFiles <- function(
-  dir = "data", 
-  reg = "^bl_dep_\\d+", 
+  dir = "data_raw",
+  reg = "^bl_dep_\\d+",
   col_spec = cols(
     ICAO24 = col_character(),
     call_sign = col_character(),
@@ -306,14 +317,10 @@ concatFiles <- function(
     arrival_date = col_date(format = ""),
     departure_airport_ICAO = col_character(),
     destination_airport_ICAO = col_character(),
-    id = col_character())
+    id = col_character()
+  )
 ) {
-
-  list.files(dir, reg, full.names = T) %>% 
-    map(~ read_csv(.x, col_types = col_spec)) %>% 
+  list.files(dir, reg, full.names = T) %>%
+    map(~ read_csv(.x, col_types = col_spec)) %>%
     bind_rows()
 }
-
-
-
-
--- R/save_historical_osn.R
+++ R/save_historical_osn.R
@@ -8,8 +8,8 @@ verbose <- T
 # timeOut <- 7
 
 
-nStartday <- 30 #32
-end <- str_c(Sys.Date()-1, " 23:59:59")
+nStartday <- 30 # 32
+end <- str_c(Sys.Date() - 1, " 23:59:59")
 start <- str_c(Sys.Date() - nStartday, " 00:00:00")
 
 qDates <- seq(as.Date(start), as.Date(end), 1)
@@ -17,102 +17,110 @@ qDates <- seq(as.Date(start), as.Date(end), 1)
 
 missingDates <- DatesMissingData(qDates)
 
-if(length(missingDates) > 0) {
-  startEnd_df <- 1:length(missingDates) %>% 
-    map_df(function(dd) 
+if (length(missingDates) > 0) {
+  startEnd_df <- seq_along(missingDates) %>%
+    map_df(function(dd) {
       tibble(
         start = str_c(missingDates[dd], " 00:00:00"),
-        end = str_c(missingDates[dd], " 23:59:59")  
-        ))
-  
-  if(verbose) cat("\nAbout to fetch data for these ", length(missingDates),
-                  " days: ",
-                  as.character(missingDates))
-  
+        end = str_c(missingDates[dd], " 23:59:59")
+      )
+    })
+
+  if (verbose) {
+    cat(
+      "\nAbout to fetch data for these ", length(missingDates),
+      " days: ",
+      as.character(missingDates)
+    )
+  }
+
   ## main
-  1:nrow(startEnd_df) %>% 
+  seq_len(nrow(startEnd_df)) %>%
     map(function(ii) {
       getSaveArrivalDeparture(
-        start = startEnd_df$start[ii], 
-        end = startEnd_df$end[ii], 
-        verbose = verbose)      
+        start = startEnd_df$start[ii],
+        end = startEnd_df$end[ii],
+        verbose = verbose
+      )
     })
 }
 
 ## concatenate everything available
-concatFiles(dir = "data", reg = "^bl_dep_\\d+",
-              col_spec = cols(
-                ICAO24 = col_character(),
-                call_sign = col_character(),
-                departure_time = col_datetime(format = ""),
-                departure_date = col_date(format = ""),
-                arrival_time = col_datetime(format = ""),
-                arrival_date = col_date(format = ""),
-                departure_airport_ICAO = col_character(),
-                destination_airport_ICAO = col_character(),
-                id = col_character())
-) %>% 
-  arrange(departure_time) %>% 
-  write_csv("data/bl_dep_all.csv")
+concatFiles(
+  dir = "data_raw", reg = "^bl_dep_\\d+",
+  col_spec = cols(
+    ICAO24 = col_character(),
+    call_sign = col_character(),
+    departure_time = col_datetime(format = ""),
+    departure_date = col_date(format = ""),
+    arrival_time = col_datetime(format = ""),
+    arrival_date = col_date(format = ""),
+    departure_airport_ICAO = col_character(),
+    destination_airport_ICAO = col_character(),
+    id = col_character()
+  )
+) %>%
+  arrange(departure_time) %>%
+  write_csv("data_raw/bl_dep_all.csv")
 
-concatFiles(dir = "data", reg = "^bl_arr_\\d+",
-            col_spec = cols(
-              ICAO24 = col_character(),
-              call_sign = col_character(),
-              departure_time = col_datetime(format = ""),
-              departure_date = col_date(format = ""),
-              arrival_time = col_datetime(format = ""),
-              arrival_date = col_date(format = ""),
-              departure_airport_ICAO = col_character(),
-              destination_airport_ICAO = col_character(),
-              id = col_character())
-) %>% 
-  arrange(departure_time) %>% 
-  write_csv("data/bl_arr_all.csv")
+concatFiles(
+  dir = "data_raw", reg = "^bl_arr_\\d+",
+  col_spec = cols(
+    ICAO24 = col_character(),
+    call_sign = col_character(),
+    departure_time = col_datetime(format = ""),
+    departure_date = col_date(format = ""),
+    arrival_time = col_datetime(format = ""),
+    arrival_date = col_date(format = ""),
+    departure_airport_ICAO = col_character(),
+    destination_airport_ICAO = col_character(),
+    id = col_character()
+  )
+) %>%
+  arrange(departure_time) %>%
+  write_csv("data_raw/bl_arr_all.csv")
 
 # STATE VECTORS
 # Take the last 4 months
-q_y_m <- seq(Sys.Date () -30 * 4, Sys.Date(), 1) %>% 
-  format("%Y_%m") %>% unique()
+q_y_m <- seq(Sys.Date() - 30 * 4, Sys.Date(), 1) %>%
+  format("%Y_%m") %>%
+  unique()
 
-concatFiles("data", 
-            str_c("^bl_dep_SV_(", str_c(q_y_m, collapse = "|"), ")"), 
-            col_spec = cols(
-  ICAO24 = col_character(),
-  longitude = col_double(),
-  latitude = col_double(),
-  requested_time = col_double(),
-  geo_altitude = col_double(),
-  velocity = col_double(),
-  special_purpose_indicator = col_logical(),
-  origin_country = col_logical(),
-  id = col_character(),
-  arrival_date = col_date(format = ""),
-  departure_date = col_date(format = "")
-)) %>% 
-  arrange(departure_date) %>% 
-  write_csv("data/bl_dep_SV_all.csv")
-
-
-concatFiles("data", 
-            str_c("^bl_arr_SV_(", str_c(q_y_m, collapse = "|"), ")"), 
-            col_spec = cols(
-              ICAO24 = col_character(),
-              longitude = col_double(),
-              latitude = col_double(),
-              requested_time = col_double(),
-              geo_altitude = col_double(),
-              velocity = col_double(),
-              special_purpose_indicator = col_logical(),
-              origin_country = col_logical(),
-              id = col_character(),
-              arrival_date = col_date(format = ""),
-              departure_date = col_date(format = "")
-            )) %>% 
-  arrange(arrival_date) %>% 
-  write_csv("data/bl_arr_SV_all.csv")
+concatFiles("data_raw",
+  str_c("^bl_dep_SV_(", str_c(q_y_m, collapse = "|"), ")"),
+  col_spec = cols(
+    ICAO24 = col_character(),
+    longitude = col_double(),
+    latitude = col_double(),
+    requested_time = col_double(),
+    geo_altitude = col_double(),
+    velocity = col_double(),
+    special_purpose_indicator = col_logical(),
+    origin_country = col_logical(),
+    id = col_character(),
+    arrival_date = col_date(format = ""),
+    departure_date = col_date(format = "")
+  )
+) %>%
+  arrange(departure_date) %>%
+  write_csv("data_raw/bl_dep_SV_all.csv")
 
 
-
-
-
+concatFiles("data_raw",
+  str_c("^bl_arr_SV_(", str_c(q_y_m, collapse = "|"), ")"),
+  col_spec = cols(
+    ICAO24 = col_character(),
+    longitude = col_double(),
+    latitude = col_double(),
+    requested_time = col_double(),
+    geo_altitude = col_double(),
+    velocity = col_double(),
+    special_purpose_indicator = col_logical(),
+    origin_country = col_logical(),
+    id = col_character(),
+    arrival_date = col_date(format = ""),
+    departure_date = col_date(format = "")
+  )
+) %>%
+  arrange(arrival_date) %>%
+  write_csv("data_raw/bl_arr_SV_all.csv")
